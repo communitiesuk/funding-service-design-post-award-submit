2e4fcd61e437999de5903236b38a94216c194226
Bobbi Brown <rholdingbrown@gmail.com>
2023-10-02 11:30:40 +0100

    WIP add copy for overdue deadline

--- app/main/utils.py
+++ app/main/utils.py
@@ -3,12 +3,12 @@ from datetime import datetime
 from config import Config
 
 
-def calculate_days_to_deadline(current_date=datetime.date(datetime.now())):
+def calculate_days_to_deadline(current_date=datetime.now().date()):
     """Calculate the number of days remaining until a specified submission deadline.
     The due_date is a str representation of submission deadline in format dd/mm/yyyy.
     It is set in main/config/envs/default.py
 
-    :param current_date: datetime object representing today's date in format yyyy-mm-dd HH:MM:SS
+    :param current_date: datetime object representing today's date in format yyyy-mm-dd
     Returns:
     :int: The number of days remaining until the submission deadline.
     """
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -20,7 +20,6 @@ def test_calculate_future_date(flask_test_client):
     assert days_remaining == 75
 
 
-# TODO consider desired behaviour if deadline date is in the past
 def test_calculate_past_date(flask_test_client, mocker):
     mock_current_date = datetime.strptime("10/09/2023", "%d/%m/%Y").date()
 
