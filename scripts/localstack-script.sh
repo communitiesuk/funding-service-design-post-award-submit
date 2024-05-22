1a98952a96ecadd5bde33c1b3525f21fc85708f7
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2024-05-22 14:30:52 +0100

    whitespace

--- scripts/localstack-script.sh
+++ scripts/localstack-script.sh
@@ -4,7 +4,6 @@ awslocal s3api \
 create-bucket --bucket data-store-failed-files-dev \
 --create-bucket-configuration LocationConstraint=eu-central-1 \
 --region eu-central-1
-
 awslocal s3api \
 create-bucket --bucket data-store-successful-files-dev \
 --create-bucket-configuration LocationConstraint=eu-central-1 \
